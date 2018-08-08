80a6f106bd8a8fdc8dac9b42fe42bf3225d6819e
Salim Qodirov <salim.qodirov@digitalinsight.com>
2018-08-07 23:17:42 -0700

    Modify and link js script.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let clock = document.getElementById('clock');
+let bgColor = document.getElementById('bg_color');
+
+function hexClock() {
+	let time = new Date();
+	
+	let hours = time.getHours().toString();
+	let minutes = time.getMinutes().toString();
+	let seconds = time.getSeconds().toString();
+
+	if (hours.length < 2) {
+		hours = '0' + hours;
+	}
+	if (minutes.length < 2) {
+		minutes = '0' + minutes;
+	}
+	if (seconds.length < 2) {
+		seconds = '0' + seconds;
+	}
+
+	let clockString = hours + ':' + minutes + ':' + seconds;
+	let bgColorString = '#' + hours + minutes + seconds;
+
+	clock.textContent = clockString;
+	bgColor.textContent = bgColorString;
+
+	document.body.style.background = bgColorString;
+}
+
+hexClock();
